1f2836bb48389bb6dca86cbdc70b6967c812694e
farias <socorro14573@gmail.com>
2024-04-26 16:21:42 -0300

    meu commit

---
+++ PartyGuestValidator.py
@@ -0,0 +1,26 @@
+# check_guest.py
+# Script para verificar se um nome está na lista de convidados e se o convidado é maior de idade.
+
+def check_guest(name, guest_list):
+    # Verifica se o nome fornecido está na lista de convidados e se o convidado é maior de idade.
+    if name in guest_list:
+        if guest_list[name] >= 18:
+            print("Acesso permitido.")
+        else:
+            print("Você foi convidado, mas é menor de idade.")
+    else:
+        print("Acesso negado. Nome não encontrado na lista.")
+
+def main():
+    # Lista de convidados autorizados com suas respectivas idades
+    guest_list = {"Alice": 25, "Bob": 17, "Charlie": 22, "Diana": 15, "Junior": 16, "Alberto": 17, "Leticia": 19}
+
+    # Solicita ao usuário que digite um nome
+    name = input("Digite o nome do convidado: ")
+
+    # Chama a função de verificação
+    check_guest(name, guest_list)
+
+if __name__ == "__main__":
+
+    main()
